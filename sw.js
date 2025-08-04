bc011987e1e9f35349db4fa57a829965ec9277f0
Alimulazad <alimulazad5@gmail.com>
2025-08-04 18:31:27 +0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+// sw.js
+const CACHE_NAME = 'biology-dictionary-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/dictionary.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
